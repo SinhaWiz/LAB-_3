f66b9115f8204cb52e13e8788dfd24434d035561
SinhaWiz <sinhartx2070@gmail.com>
2024-09-24 23:28:10 +0600

    Test Cases for number word check done

--- out/src/Tests/NumWordPrinterTest.java
+++ out/src/Tests/NumWordPrinterTest.java
@@ -1,4 +1,36 @@
 package Tests;
-
+import Tasks.NumWordPrinter;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class NumWordPrinterTest {
+    @Test
+    public void testEvenPrimeLucky() {
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("EVENPRIMELUCKY", printer.printWord(286));
+    }
+    @Test
+    public void testEvenPrimeEleven() {
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("EvenPrimeEleven", printer.printWord(22));
+    }
+    @Test
+    public void testEven() {
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("Even", printer.printWord(4));
+    }
+    @Test
+    public void testPrimeEleven() {
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("PrimeEleven", printer.printWord(11));
+    }
+    @Test
+    public void testLuckyThirteen() {
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("LuckyThirteen", printer.printWord(13));
+    }
+    @Test
+    public void testOddOneOut() {
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("OddOneOut", printer.printWord(7));
+    }
 }
