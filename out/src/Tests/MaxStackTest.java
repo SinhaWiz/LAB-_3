b213558fac85c40bc0eb9d03f5ddbf431dbd1801
SinhaWiz <sinhartx2070@gmail.com>
2024-09-24 23:22:45 +0600

    Push And Max test

--- out/src/Tests/MaxStackTest.java
+++ out/src/Tests/MaxStackTest.java
@@ -1,4 +1,17 @@
 package Tests;
 
+import Tasks.MaxStack;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MaxStackTest {
+    @Test
+    public void testPushAndMax() {
+        MaxStack stack = new MaxStack();
+        stack.push(3);
+        stack.push(2);
+        stack.push(5);
+        stack.push(6);
+        assertEquals(6, stack.max());
+    }
 }
