0a8b7fe05c716bb43a1e951480a65d4dd7c38ce4
SinhaWiz <sinhartx2070@gmail.com>
2024-09-24 23:24:25 +0600

    Min value Test

--- out/src/Tests/MaxStackTest.java
+++ out/src/Tests/MaxStackTest.java
@@ -14,4 +14,24 @@ public class MaxStackTest {
         stack.push(6);
         assertEquals(6, stack.max());
     }
+
+    @org.junit.jupiter.api.Test
+    public void testPopAndMax() {
+        MaxStack stack = new MaxStack();
+        stack.push(3);
+        stack.push(2);
+        stack.push(5);
+        stack.push(6);
+        stack.pop();
+        assertEquals(5, stack.max());
+    }
+    @org.junit.jupiter.api.Test
+    public void testMin() {
+        MaxStack stack = new MaxStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(2);
+        assertEquals(1, stack.min());
+    }
 }
