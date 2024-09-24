cb74f96361d71b245b7650a830890933298d7e11
SinhaWiz <sinhartx2070@gmail.com>
2024-09-24 23:13:09 +0600

    pop function ready

--- out/src/Tasks/MaxStack.java
+++ out/src/Tasks/MaxStack.java
@@ -1,4 +1,30 @@
 package Tasks;
 
+import java.util.Stack;
 public class MaxStack {
+    private Stack<Integer> mainStack;
+    private Stack<Integer> maxStack;
+    public MaxStack() {
+        mainStack = new Stack<>();
+        maxStack = new Stack<>();
+    }
+    public void push(int value) {
+        mainStack.push(value);
+        if (maxStack.isEmpty() || value >= maxStack.peek()) {
+            maxStack.push(value);
+        }
+    }
+//    public int pop() {
+//        int value = mainStack.pop();
+//        if (value == maxStack.peek()) {
+//            maxStack.pop();
+//        }
+//        return value;
+//    }
+    public int max() {
+        return maxStack.peek();
+    }
+    public int min() {
+        return mainStack.stream().min(Integer::compare).orElseThrow();
+    }
 }
--- out/src/Tasks/NumWordPrinter.java
+++ out/src/Tasks/NumWordPrinter.java
@@ -1,4 +1,23 @@
 package Tasks;
-
 public class NumWordPrinter {
+    public String printWord(int number) {
+        String result;
+        if (isDivisibleBy(number, 2) && isDivisibleBy(number, 11) && isDivisibleBy(number, 13)) {
+            result = "EvenPrimeLucky";
+        } else if (isDivisibleBy(number, 2) && isDivisibleBy(number, 11)) {
+            result = "EvenPrimeEleven";
+        } else if (isDivisibleBy(number, 2)) {
+            result = "Even";
+        } else if (isDivisibleBy(number, 11)) {
+            result = "PrimeEleven";
+        } else if (isDivisibleBy(number, 13)) {
+            result = "LuckyThirteen";
+        } else {
+            result = "OddOneOut";
+        }
+        return  result;
+    }
+    private boolean isDivisibleBy(int number, int divisor) {
+        return number % divisor == 0;
+    }
 }
